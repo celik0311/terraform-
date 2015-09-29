19baa9f4cc3b0d165227fdbb06358e12c8b94e3d
Celik Sismanturk <celik@knewton.com>
2015-09-29 19:32:26 -0400

    initial setup

---
+++ provider-variables.tf
@@ -0,0 +1,7 @@
+variable "provider" {
+    default = {
+        access_key = "not undefined yet"
+        secret_key = "not undefined yet"
+        region     = "us-east-1"
+    }
+}
---
+++ vpc-variables.tf
@@ -0,0 +1,6 @@
+variable "vpc" {
+    default = {
+        owner_id = "unknown"
+        cidr_block = "XX.X.X.X/XX"
+    }
+}
